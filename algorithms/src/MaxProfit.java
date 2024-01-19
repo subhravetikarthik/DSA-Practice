ade4a7d6dfb405f6ffbcf8e7b448fd780046ed62
karthiks <subhravetikarthik@gmail.com>
2024-01-19 20:21:53 +0530

    solve buy and sell stock.

---
+++ algorithms/src/MaxProfit.java
@@ -0,0 +1,26 @@
+/**
+ * You are given an array prices where prices[i] is the price of a given stock on the ith day.
+ * You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+ * Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+ */
+
+public class MaxProfit {
+    public static void main(String[] args) {
+        int[] prices = {2,4,1};
+        int res = maxProfit(prices);
+        System.out.println(res);
+    }
+
+    public static int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+}
